038de969eda5305ffc9acf3aebb63021675103e0
NicoAAA <nicolas.acosta@soy.sena.edu.co>
2025-11-12 16:42:57 -0500

    Second Commit: Agregando Logos Nuevos, informacion de metadatos, imagen y nombre en parte superior de la pagina web

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,12 @@ export default {
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // Creamos una clase nueva 'font-brand' solo para items en el navbar
+        brand: ['Montserrat', 'sans-serif'], 
+      },
+    },
   },
   plugins: [],
 }
