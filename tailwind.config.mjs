4fb9570232e4f7c3765d82ed688e5d3c8d6dddd5
NicoAAA <nicolas.acosta@soy.sena.edu.co>
2025-11-13 14:43:53 -0500

    Actualizacion de paleta de colores y correccion del tamaño del logo en web

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,6 +9,23 @@ export default {
         // Creamos una clase nueva 'font-brand' solo para items en el navbar
         brand: ['Montserrat', 'sans-serif'], 
       },
+      colors: {
+        // Tus colores personalizados
+        brand: {
+          DEFAULT: '#a5bba7', // Tu color base (bg-brand)
+          light: '#c5d6c7',   // Para fondos muy suaves
+          dark: '#4a5e4d',    // Para textos sobre fondos claros o bordes
+        },
+        // Colores semánticos para el modo oscuro/claro
+        surface: {
+          light: '#ffffff',
+          dark: '#1f2937', // Gray-800
+        },
+        canvas: {
+          light: '#f9fafb', // Gray-50
+          dark: '#111827',  // Gray-900
+        }
+      },
     },
   },
   plugins: [],
