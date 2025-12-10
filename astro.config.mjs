6d672c8851941ee1f95fb06415722f390fe2967c
NicoAAA <nicolas.acosta@soy.sena.edu.co>
2025-12-10 09:57:03 -0500

    Configuracion de Publicaciones en T3 Fashion News usando saniti.io desde la nube

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind"; // <-- El import CORRECTO
 
+import sanity from "@sanity/astro";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()] // <-- La configuración CORRECTA
+  integrations: [
+    tailwind(), 
+    sanity({
+      projectId: "9fioqt11",
+      dataset: "production",
+      useCdn: true,
+    })
+  ] 
 });
